ca6a6db50a0710661b1932b743230c75e2bb82de
apw516 <redhawk5665@gmail.com>
2026-05-22 23:02:54 +0700

    ad

--- resources/views/Kasir/index.blade.php
+++ resources/views/Kasir/index.blade.php
@@ -86,6 +86,8 @@
             <div class="card mt-3">
                 <div class="card-header">Silahkan Pilih Obat</div>
                 <div class="card-body">
+                    <button hidden class="btn btn-success mb-2" data-bs-toggle="modal" data-bs-target="#modaladdmasterbarang"><i
+                            class="bi bi-folder-plus" style="margin-right:12px"></i> Master Barang</button>
                     <table class="table table-bordered table-hover table-sm" id="table-barang">
                         <thead>
                             <tr>
@@ -160,6 +162,24 @@
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
                     <button type="button" class="btn btn-primary" onclick="simpansesikasir()">Simpan</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="modaladdmasterbarang" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
         </div>
